e6e092c153468da910211ff50cfb213a4008f215
Zepticona <67518532+Zepticona@users.noreply.github.com>
2020-10-05 11:20:44 +0600

    heroku config vars added

--- index.js
+++ index.js
@@ -108,4 +108,4 @@ client.connect(err => {
 
 
 // Initializing Express server
-app.listen(8080, () => console.log('Server running at port 8080'))
+app.listen(process.env.PORT || 8080, () => console.log('Server running at port 8080'))
